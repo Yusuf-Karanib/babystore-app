614755647df8ac9c62849ec1cd162a4d5d1c6513
Courier <Yusufkaranib91k@gmail.com>
2026-03-28 13:53:18 +0400

    x

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-pip3 install -r requirements.txt
-pkill -f app.py || true
+sudo pip3 install -r requirements.txt
+sudo pkill -f app.py || true
 sudo nohup python3 app.py > app.log 2>&1 &
